306ffb24111ac641576b989bbcf974062ce183ee
ik340 <isuriudani2004@gmail.com>
2025-10-04 15:38:46 +0530

    Update TicTacToepart1.c

--- TicTacToepart1.c
+++ TicTacToepart1.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "TicTacToe.h"
 
-#define MIN_SIZE 3
-#define MAX_SIZE 10
-
-// Function prototypes
-char *initBoard(int size);
-void displayBoard(char *board, int size);
-void getUserInput(int *row, int *col, int size, char player);
-int isValidMove(int row, int col, int size, char *board);
-int checkWin(char *board, int size, char symbol);
-int checkDraw(char *board, int size);
-void logMove(FILE *log, char *board, int size, int row, int col, char symbol);
 
 int main() {
 	int size;
