39754ec92aeccba11b04c232be69a4d06f2a0505
ik340 <isuriudani2004@gmail.com>
2025-10-05 17:22:39 +0530

    Update TicTacToe.h

--- TicTacToe.h
+++ TicTacToe.h
@@ -6,7 +6,7 @@
 #define MIN_SIZE 3
 #define MAX_SIZE 10
 
-// Function prototypes
+// Function prototypes for the board
 char *initBoard(int size);
 void displayBoard(char *board, int size);
 void getUserInput(int *row, int *col, int size, char player);
@@ -16,5 +16,11 @@ int checkWin(char *board, int size, char symbol);
 int checkDraw(char *board, int size);
 void logMove(FILE *log, char *board, int size, int row, int col, char symbol);
 
+// Function prototypes for to select the game mode
+void twoPlayers(int size);  // User vs User
+void userVsComputer(int size);  // user vs computer
+void threePlayers(int size);   // three players
+
 #endif
+
 
